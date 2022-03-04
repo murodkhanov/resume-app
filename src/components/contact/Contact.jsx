108439ef994af425dcdc511399728d6bf3c34d07
Dilmurod Akhmedov <mrdilmurodakhmedov@gmail.com>
2022-03-04 16:47:26 +0500

    T C

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,76 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { RiTelegramLine } from "react-icons/ri";
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
-  )
-}
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
 
-export default Contact
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>mrdilmurodakhmedov@gmail.com</h5>
+            <a
+              href="mailto:mrdilmurodakhmedov@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messanger</h4>
+            <h5>Dilmurod Akhmedov</h5>
+            <a
+              href="https://www.facebook.com/dilmurod.axmedov.927"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiTelegramLine className="contact__option-icon" />
+            <h4>Telegram</h4>
+            <h5>+998 93 299 88 22</h5>
+            <a href="https://t.me/murodkhanov" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        {/* END OF CONTACT OPTIONS */}
+
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,75 @@
-import React from 'react';
-import './testimonials.css';
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar-3.png";
+import AVTR2 from "../../assets/avatar-3.png";
+import AVTR3 from "../../assets/avatar-3.png";
+import AVTR4 from "../../assets/avatar-3.png";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "First Client",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex!",
+  },
+  {
+    avatar: AVTR2,
+    name: "Second Client",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex!",
+  },
+  {
+    avatar: AVTR3,
+    name: "Third Client",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex!",
+  },
+  {
+    avatar: AVTR4,
+    name: "Fouth Client",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, ipsam! Veniam velit dolore repellendus quos et nam minus tenetur ipsam, libero nisi ab? Harum quo optio libero laboriosam odit ex!",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonias</h2>
 
-export default Testimonials
+      <Swiper
+        className="container testimonials__container" // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatars" />
+              </div>
+
+              <h5 className="client__name">{name}</h5>
+
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
