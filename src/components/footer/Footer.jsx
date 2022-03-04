29046753350676d6915786dadc8ea7be3cf99caa
Dilmurod Akhmedov <mrdilmurodakhmedov@gmail.com>
2022-03-04 23:52:31 +0500

    Finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <a href="!#" className="footer__logo">
+        Murod Khanov
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="!#">Home</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#srvices">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.facebook.com/dilmurod.axmedov.927"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://www.instagram.com/murod.khanov/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dilmurod-akhmedov-7b4962197/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedinIn />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Murod Khanov. All right reserved.</small>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
