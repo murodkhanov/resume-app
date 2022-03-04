ddb4c6ed139e0c506845506a2c82271d70d43011
Dilmurod Akhmedov <mrdilmurodakhmedov@gmail.com>
2022-03-05 02:16:17 +0500

    Social Links Added

--- src/components/header/HeaderSocias.jsx
+++ src/components/header/HeaderSocias.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa';
-import {FiDribbble} from 'react-icons/fi';
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocias = () => {
   return (
-    <div className='header__socials'>
-        <a href="htpps://linkedin.com"  rel="noreferrer noopener" target="_blank"><BsLinkedin/></a>
-        <a href="htpps://github.com" rel="noreferrer noopener" target="_blank"><FaGithub/></a>
-        <a href="htpps://dribbble.com" rel="noreferrer noopener" target="_blank"><FiDribbble/></a>
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/dilmurod-akhmedov-7b4962197/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/murodkhanov/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        <FaGithub />
+      </a>
+      <a href="htpps://dribbble.com" rel="noreferrer noopener" target="_blank">
+        <FiDribbble />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocias
+export default HeaderSocias;
