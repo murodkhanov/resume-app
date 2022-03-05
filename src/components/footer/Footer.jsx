3fd8176c64ab16af3e8788fd266c40ed74572920
Dilmurod Akhmedov <mrdilmurodakhmedov@gmail.com>
2022-03-05 22:42:22 +0500

    Footer Links Changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,16 +13,16 @@ function Footer() {
 
       <ul className="permalinks">
         <li>
-          <a href="!#">Home</a>
+          <a href="/">Home</a>
         </li>
         <li>
-          <a href="about">About</a>
+          <a href="#about">About</a>
         </li>
         <li>
           <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="#srvices">Services</a>
+          <a href="#services">Services</a>
         </li>
         <li>
           <a href="#portfolio">Portfolio</a>
